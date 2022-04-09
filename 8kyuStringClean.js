83e0f9819781da33bdedea16e864b5bef1b6d922
thaoly258 <joly8147@gmail.com>
2022-04-08 23:33:33 -0700

    daily codewars

---
+++ 8kyuStringClean.js
@@ -0,0 +1,11 @@
+//Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+//Examples (input -> output)
+//'! !'                 -> '! !'
+//'123456789'           -> ''
+//'This looks5 grea8t!' -> 'This looks great!'
+
+function stringClean(s){
+    return s.replace(/[0-9]/g, '')
+  }
+  
