845c82e1c852ec6bf829969ff7c42587feaca5d7
thaoly258 <joly8147@gmail.com>
2022-04-17 22:45:44 -0700

    daily codewars

---
+++ 8kyuNameShuffler.js
@@ -0,0 +1,6 @@
+//Write a function that returns a string in which firstname is swapped with last name.
+//
+//nmeShuffler('john McClane'); => "McClane john"
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
