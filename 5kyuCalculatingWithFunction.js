73a91ffc4da2f0a0ac65fa784ef669d7a5fdb604
thaoly258 <joly8147@gmail.com>
2022-05-05 21:01:29 -0700

    daily codewars

---
+++ 5kyuCalculatingWithFunction.js
@@ -0,0 +1,33 @@
+const zero = (operation) => !operation ? 0 : operation(0);
+
+
+const one = (operation) => !operation ? 1 : operation(1);
+
+
+const two = (operation) => !operation ? 2 : operation(2);
+
+
+const three = (operation) => !operation ? 3 : operation(3);
+
+
+const four = (operation) => !operation ? 4 : operation(4);
+
+
+const five = (operation) => !operation ? 5 : operation(5);
+
+
+const six = (operation) => !operation ? 6 : operation(6);
+
+
+const seven = (operation) => !operation ? 7 : operation(7);
+
+
+const eight = (operation) => !operation ? 8 : operation(8);
+
+
+const nine = (operation) => !operation ? 9 : operation(9);
+
+const plus = (number) => (other_number) => Math.floor(other_number + number);
+const minus = (number) => (other_number) => Math.floor(other_number - number);
+const times = (number) => (other_number) => Math.floor(other_number * number);
+const dividedBy = (number) => (other_number) => Math.floor(other_number / number);
