7dfe7525d5cb547830e464cce2bf56b2b50cd366
thaoly258 <joly8147@gmail.com>
2022-05-06 22:58:47 -0700

    daily codewars

---
+++ 7kyuRoundUpToTheNext5.js
@@ -0,0 +1,10 @@
+//Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+function roundToNext5(n){
+    return Math.ceil(n/5)*5;
+  }
+
+or
+function roundToNext5(n){
+    while(n % 5 !== 0) n++
+    return n
+  }
---
+++ 7kyuSumOfOddNumber.js
@@ -0,0 +1,13 @@
+//Given the triangle of consecutive odd numbers:
+//
+//             1
+//          3     5
+//       7     9    11
+//   13    15    17    19
+//21    23    25    27    29
+//...
+//Calculate the sum of the numbers in the nth row of this triangle
+
+function rowSumOddNumbers(n) {
+    return Math.pow(n, 3);
+  }
