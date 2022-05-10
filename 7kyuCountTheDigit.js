b096f3bbf4e0d7c5a9d0fcf8357e68c88077bcf4
thaoly258 <joly8147@gmail.com>
2022-05-09 21:02:06 -0700

    daily codewars

---
+++ 7kyuCountTheDigit.js
@@ -0,0 +1,16 @@
+//Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+//
+//Square all numbers k (0 <= k <= n) between 0 and n.
+//
+//Count the numbers of digits d used in the writing of all the k**2.
+//
+//Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+function nbDig(n, d) {
+    let count = 0;
+  
+    for(let i = 0; i <= n; i++) {
+      count += (i*i).toString().split('').filter(n => n == d).length;
+    }
+      
+    return count
+}
