f61cb46990cdf202878a3a96b63b49f752129901
thaoly258 <joly8147@gmail.com>
2022-05-29 17:04:05 -0700

    daily codewars

---
+++ 8kyuSuperDuperEasy.js
@@ -0,0 +1,3 @@
+//Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+const problem = x => 
+ Number(x) === x ? x*50 + 6 : 'Error'
