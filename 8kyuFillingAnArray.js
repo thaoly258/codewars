034f42c53b6bd417f272369af29788a8cb36180c
thaoly258 <joly8147@gmail.com>
2022-06-03 20:22:33 -0700

    daily codewars

---
+++ 8kyuFillingAnArray.js
@@ -0,0 +1,7 @@
+//Write a function that produces an array with the numbers 0 to N-1 in it.
+//
+//For example, the following code will result in an array containing the numbers 0 to 4:
+//
+//arr(5) // => [0,1,2,3,4]
+
+const arr = N => Array.from({length: N}, (v,k) => k)
