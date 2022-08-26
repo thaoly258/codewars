718b746a3f196281d5ce8cb57450f004242a194d
thaoly258 <joly8147@gmail.com>
2022-08-25 17:28:06 -0700

    daily codewars

---
+++ 6kyuSplitString.js
@@ -0,0 +1,16 @@
+//Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//
+//Examples:
+//
+//* 'abc' =>  ['ab', 'c_']
+//* 'abcdef' => ['ab', 'cd', 'ef']
+
+
+function solution(str){
+    if(str.length % 2 !== 0) str+='_'
+    let res = []
+    for(let i=0; i<str.length; i+=2){
+      res.push(str.slice(i,i+2))
+    }
+    return res   
+  }
